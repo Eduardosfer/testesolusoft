8483a7cd61e350c54d73a17ba35463467dafba29
Eduardosfer <eduardosfer@outlook.com>
2019-02-09 15:18:16 -0300

    Criação do metodo de getPedidosPorCliente no modelo

--- application/models/Relatorios_Model.php
+++ application/models/Relatorios_Model.php
@@ -30,4 +30,17 @@ class Relatorios_Model extends MY_Model {
 		return $object->result();
 	}
 
+	public function getPedidosPorCliente($ped_codigo_cliente = null) {
+		$this->db->select("pedidos.*, pedidos.ped_codigo as ped_codigo_atual");
+		$this->db->select("clientes.cli_nome");
+		$this->db->select("vendedores.ven_nome");
+		$this->db->select("(select count(pro_ped_codigo) from produtos_pedido where pro_ped_codigo_pedido = ped_codigo_atual) as total_produtos");
+		$this->db->select("(select sum(pro_valor) from produtos, produtos_pedido where pro_codigo = pro_ped_codigo_produto and pro_ped_codigo_pedido = ped_codigo_atual) as valor_total_produtos");
+		$this->db->where("ped_codigo_cliente", $ped_codigo_cliente);		
+		$this->db->join("clientes", "clientes.cli_codigo = $this->table.ped_codigo_cliente", "LEFT");		
+		$this->db->join("vendedores", "vendedores.ven_codigo = $this->table.ped_codigo_vendedor", "LEFT");
+		$object = $this->db->get($this->table);
+		return $object->result();
+	}
+
 }
