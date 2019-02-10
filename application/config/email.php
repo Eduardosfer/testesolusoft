50457721e3af17c23e2d10dbb7509a5ef4960f46
Eduardosfer <eduardosfer@outlook.com>
2019-02-10 00:41:13 -0300

    Criação do arquivo de config de email.php para as configurações do email

---
+++ application/config/email.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['smtp_host'] = 'ssl://smtp.gmail.com';
+$config['smtp_port'] = 465;
+$config['smtp_user'] = 'emailparatestemail@gmail.com';
+$config['smtp_pass'] = '@e12345678';
+$config['protocol']  = 'smtp';
+$config['validate']  = TRUE;
+$config['mailtype']  = 'text';
+$config['charset']   = 'utf-8';
+$config['newline']   = "\r\n";
