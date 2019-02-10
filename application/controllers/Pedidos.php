0e5e926b75cb0bae744b425dd91c8ef4688e8496
Eduardosfer <eduardosfer@outlook.com>
2019-02-10 01:46:10 -0300

    Metodo de enviarEmail finalizado + configurarEmail e configurarMensagem. Envio de pedido para email do cliente finalizado

--- application/controllers/Pedidos.php
+++ application/controllers/Pedidos.php
@@ -117,11 +117,84 @@ class Pedidos extends CI_Controller {
 	public function enviarEmail() {
 		$where = array("ped_codigo" => $this->input->post('ped_codigo'));				
 		$this->load->model('Pedidos_Model');
-		$pedido_edicao = $this->Pedidos_Model->getOneData($where);
+		$pedido = $this->Pedidos_Model->getOneDataJoined($where);
 		// Obtendo os itens do pedido
 		$this->load->model('Produtos_Pedido_Model');
-		$pedido_edicao->itens_do_pedido = $this->Produtos_Pedido_Model->getDataJoined(array("pro_ped_codigo_pedido" => $where['ped_codigo']));		
+		$pedido->itens_do_pedido = $this->Produtos_Pedido_Model->getDataJoined(array("pro_ped_codigo_pedido" => $where['ped_codigo']));		
 		// if validação
-		echo json_encode(array("menssagem" => "success"));
+		$this->configurarEmail($pedido);
+		echo json_encode(array("menssagem" => "success"));		
 	}
+
+	public function configurarEmail($pedido = null){
+		$this->load->library('email');
+		$this->email->from("emailparatestemail@gmail.com", 'Email de teste sistema');
+		$this->email->subject("Pedido nº $pedido->ped_codigo");
+		// $this->email->reply_to("naorespondaesseemail@gmail.com");
+		// $this->email->cc('copia@dominio.com');
+		// $this->email->bcc('copia_oculta@dominio.com');		
+		$menssagem = $this->configurarMenssagem($pedido);		
+		$this->email->to($pedido->cli_email);
+		$this->email->message($menssagem);
+		$this->email->send();
+	}
+
+	public function configurarMenssagem($pedido = null) {
+		$data = date('d/m/Y', strtotime($pedido->ped_data));
+		$itens_pedido = "";
+		foreach ($pedido->itens_do_pedido as $item) {
+			$itens_pedido .= "<tr><td>$item->pro_codigo</td><td>$item->pro_nome</td><td>$item->pro_cor</td><td>$item->pro_tamanho</td><td>R$ $item->pro_valor</td></tr>";
+		}
+		$menssagem = "
+						<html>
+							<head>
+								<style>
+									table {
+										padding: 10px;
+										font-size: 15px;
+									}
+									td {
+										padding: 5px;
+									}														
+								</style>
+							</head>
+							<body>
+								<h3>Dados do pedido $pedido->ped_codigo</h3>
+								<table>
+									<tbody>
+										<tr>
+											<td>Cliente: $pedido->cli_nome</td>
+											<td>Vendedor: $pedido->ven_nome</td>
+										</tr>
+										<tr>
+											<td>Forma de pagamento: $pedido->ped_forma_pagamento</td>
+											<td>Data do pedido: $data</td>
+										</tr>
+										<tr>
+											<td>Observação: $pedido->ped_observacao</td>			
+										</tr>
+									</tbody>
+								</table>
+								<h3>Itens do pedido</h3>
+								<table>
+									<thead>
+										<tr>
+											<th>Código</th>
+											<th>Nóme</th>
+											<th>Cor</th>
+											<th>Tamanho</th>
+											<th>Valor</th>
+										</tr>
+									</thead>
+									<tbody>
+										$itens_pedido
+									</tbody>
+								</table>
+							</body>
+						</html>						
+					";
+
+		return $menssagem;
+	}	
+
 }
