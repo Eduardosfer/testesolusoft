ad92d0e2938abf341740a62f87b7e77c9a964b2e
Eduardosfer <eduardosfer@outlook.com>
2019-02-10 03:14:09 -0300

    Criação da view dados_pedido que contem um formulário com os dados do pedido que é utilizado para enviar por email e posteriormente para ser impresso

---
+++ application/views/pedidos/dados_pedido.php
@@ -0,0 +1,58 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<html>
+<head>
+	<style>
+		table {
+			padding: 10px;
+			font-size: 15px;
+		}
+		td {
+			padding: 10px;
+		}														
+	</style>
+</head>
+<body>
+	<h3>Dados do pedido <?= $pedido->ped_codigo ?></h3>
+	<table>
+		<tbody>
+			<tr>
+				<td>Cliente: <?= $pedido->cli_nome ?></td>
+				<td>Vendedor: <?= $pedido->ven_nome ?></td>
+			</tr>
+			<tr>
+				<td>Forma de pagamento: <?= $pedido->ped_forma_pagamento ?></td>
+				<td>Data do pedido: <?= date('d/m/Y', strtotime($pedido->ped_data)) ?></td>
+			</tr>
+			<tr>
+				<td>Observação: <?= $pedido->ped_observacao ?></td>			
+			</tr>
+		</tbody>
+	</table>
+	<h3>Itens do pedido</h3>
+	<table>
+		<thead>
+			<tr>
+				<th>Código</th>
+				<th>Nóme</th>
+				<th>Cor</th>
+				<th>Tamanho</th>
+				<th>Valor</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach ($pedido->itens_do_pedido as $item) { ?>
+				<tr>
+					<td><?= $item->pro_codigo ?></td>
+					<td><?= $item->pro_nome ?></td>
+					<td><?= $item->pro_cor ?></td>
+					<td><?= $item->pro_tamanho ?></td>
+					<td><?= "R$ " . $item->pro_valor ?></td>
+				</tr>
+			<?php } ?>
+		</tbody>
+	</table>
+</body>
+</html>	
